855524f4c53d303c4c48d306629810abfc7034fb
Albeiro Crespo <alcresgu_1989@hotmail.com>
2023-02-27 23:36:10 -0500

    implement flyweight use case

---
+++ src/structural/flyweight/useCase/flyweightFactory.ts
@@ -0,0 +1,20 @@
+import Flyweight from "./flyweight";
+
+export default class FlyweightFactory {
+  // Creating the FlyweightFactory as a static class
+  static flyweights: { [id: number]: Flyweight } = {};
+
+  static getFlyweight(code: number): Flyweight {
+    // A static method to get a flyweight based on code
+    if(!(code in FlyweightFactory.flyweights)) {
+      return FlyweightFactory.flyweights[code];
+    }
+
+    return FlyweightFactory.flyweights[code];
+  }
+
+  static getCount(): number {
+    // Returns the number of flyweights in the cache
+    return Object.keys(FlyweightFactory.flyweights).length;
+  }
+}
